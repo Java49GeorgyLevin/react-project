3005d13981e8bb60a0e81434d7a1cc8ebe54618b
Georgy Levin <g.phyzy@gmail.com>
2023-01-27 10:09:25 +0200

    getRandomMatrix? getRandomArrayElement?

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { getRandomNumber } from './components/utils/random';
+import { getRandomDate, getRandomNumber } from './components/utils/random';
 import { getRandomMatrix } from './components/utils/random';
+import { getRandomArrayElement } from './components/utils/random';
 
 function App() {
  
-  console.log(getRandomMatrix(5, 6, 2, 8));
+  // console.log(getRandomMatrix(5, 6, 2, 8));
+  // getRandomMatrix(5, 6, 2, 8)
 
+  // console.log(getRandomArrayElement([5, 6, 2, 8, 5, 6, 2, 8]));
+  getRandomDate(1970, 1980);
   return <div></div>
 }
 
--- src/components/utils/random.ts
+++ src/components/utils/random.ts
@@ -15,33 +15,50 @@ export function getRandomNumber(min: number, max: number,
 
     
  export function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][] {
-    let rndMatrix: number[][] = new Array();
+    let rndMatrix: number[][] = new Array([rows], [columns]);
+    // console.log(rndMatrix);
+    // return rndMatrix.map(getRandomNumber(min, max, true, true));
+    // }
     // let rowsMatrix: number[] = [];
     for(let i = 0; i < rows; i ++) {
-        // rndMatrix[i] = getRandomNumber(min, max, true, true);
+    //     rndMatrix[i] = getRandomNumber(min, max, true, true);
     // }
     // let columnsMatrix: number[] = [];
         for(let j = 0; j < columns; j ++) {
         // columnsMatrix.push(getRandomNumber(min, max, true, true));
         // rndMatrix[i][j] = getRandomNumber(min, max, true, true);
-            rndMatrix[i][j] = getRandomNumber(min, max, true, true);
+        //   rndMatrix[i[j] = getRandomNumber(min, max, true, true);
+        //   console.log(rndMatrix);
     }
 }
     // 5.2.1.Returns a random matrix containing “rows” rows and “columns” columns, 
     // where each element is a random number in the closed range [min, max] (min and max are inclusive)
-
-    return rndMatrix;
-
+    // console.log(rndMatrix);
+    return [];
     }
 
 export function getRandomArrayElement(array: any[]): any {
+    console.log(array);
+    console.log(array.length);
     const n: number = getRandomNumber(0, array.length, true, false);
+    console.log("n", n)
     return array[n];
 
 }
     
-// export function getRandomDate(minYear: number, maxYear: number): Date
+export function getRandomDate(minYear: number, maxYear: number): Date {
+
+    const rndYear = getRandomNumber(minYear, maxYear, true, false);
+    const rndMonth = getRandomNumber(1, 12, true, true);
+    const rndDay = getRandomNumber(1, 31, true, true);
+    const rndDate = new Date(rndYear, rndMonth, rndDay);
     // 5.4.1.Returns random date object (see documentation of class Date)
     // 5.4.1.1.Random year from the range [minYear, maxYear) minYear inclusive, maxYear -enclusive
-    // 5.4.1.2.Random month5.
+    // 5.4.1.2.Random month.
     //     4.1.3.Randomday
+    console.log(rndDate);
+    return rndDate;
+
+
+}
+    // export default getRandomNumber;
