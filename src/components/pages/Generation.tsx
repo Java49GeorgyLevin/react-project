21c0d0cf9c48bf7872a5d832803b3d716b8c13c3
Georgy Levin <g.phyzy@gmail.com>
2023-02-25 18:13:19 +0200

    HW44

--- src/components/pages/Generation.tsx
+++ src/components/pages/Generation.tsx
@@ -6,10 +6,13 @@ import { createRandomEmployee } from "../../service/EmployeesService";
 import { employeesActions } from "../../redux/employees-slice";
 import { useDispatch } from "react-redux";
 import { Employee } from "../../model/Employee";
+import { Alert, AlertTitle } from "@mui/material";
 const minAmount = 1;
 const maxAmount = 50;
+const timeout = 4000;
 
 export const Generation: React.FC = () => {
+    const [flAlert, setFlAlert] = useState<boolean>(false);
     const dispatch = useDispatch();
     const [amount, setAmount] = useState(0);
 
@@ -21,6 +24,8 @@ export const Generation: React.FC = () => {
         for(let i = 0;i < amount;i++) {
             const newEmployee: Employee = createRandomEmployee();
             dispatch(employeesActions.addEmployee(newEmployee));
+            setFlAlert(true);
+            setTimeout(() => setFlAlert(false), timeout);
         }
         document.querySelector('form')!.reset();
     }
@@ -34,6 +39,10 @@ export const Generation: React.FC = () => {
             }}>
             </TextField>
             <Button type='submit'>Submit</Button>
-        </form>
+        </form>        
+        {flAlert && <Alert severity="success">
+            <AlertTitle>Successfully</AlertTitle>
+            added {amount} employee.
+        </Alert>}
     </Box>
 }
